cc1b643e62a87025e49352b65bcf31fb7e8efea0
nesssery <nesssery@gmail.com>
2020-12-19 18:12:40 +0000

    Commit real-world-vue-frontend

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://real-world-vue-gacq.onrender.com/api",
   withCredentials: false,
   header: {
     "Access-Control-Allow-Origin": "*",
